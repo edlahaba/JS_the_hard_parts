dc2f7805e1c0dd294c77127cdcd47a4ff59416a5
elacalle <elacalle@nosolosoftware.es>
2019-12-21 18:04:57 +0100

    Fixed formatting
    Fixed code formatting to removing tab spaces by two spces

--- ex1_js_hardparts.js
+++ ex1_js_hardparts.js
@@ -1,6 +1,6 @@
 // Challenge 1
 function addTwo(num) {
-	return num + 2;
+  return num + 2;
 }
 
 // To check if you've completed it, uncomment these console.logs!
@@ -10,7 +10,7 @@ console.log(addTwo(10));
 
 // Challenge 2
 function addS(word) {
-	return `${word}s`;
+  return `${word}s`;
 }
 
 // uncomment these to check your work
@@ -20,7 +20,7 @@ console.log(addS('bagel'));
 
 // Challenge 3
 function map(array, callback) {
-	return array.map(callback);
+  return array.map(callback);
 }
 
 console.log(map([1, 2, 3], addTwo));
@@ -28,7 +28,7 @@ console.log(map([1, 2, 3], addTwo));
 
 // Challenge 4
 function forEach(array, callback) {
-	array.forEach(callback)
+  array.forEach(callback)
 }
 
 // see for yourself if your forEach works!
@@ -59,10 +59,10 @@ console.log(reduce([1,2,3], (x,y) => { return x + y; }, 0));
 function intersection(...arrays) {
   var result = {};
 
-	reduce(arrays, (result, array) => {
-  	array.forEach(number => {
-     result[number] = result[number] || [];
-     result[number].push(number)
+  reduce(arrays, (result, array) => {
+    array.forEach(number => {
+      result[number] = result[number] || [];
+      result[number].push(number)
     });
 
     return result;
@@ -78,8 +78,8 @@ console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])
 function union(...arrays) {
   var result = {};
 
-	reduce(arrays, (result, array) => {
-  	array.forEach(number => { result[number] = result[number] || true; });
+  reduce(arrays, (result, array) => {
+    array.forEach(number => { result[number] = result[number] || true; });
 
     return result;
   }, result);
@@ -87,7 +87,7 @@ function union(...arrays) {
   return Object.keys(result);
 }
 
- console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
+console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
 // should log: [5, 10, 15, 88, 1, 7, 100]
 
 //Extension 5
